05d791da45b673354fde2edaffc45ae0eb13bf07
shraddhaK123 <shraddha.kodam@sodelsolutions.com>
2025-01-04 19:36:27 +0530

    admin_user migraton added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # Devise routes for admin users
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
+  # ActiveAdmin routes
+  ActiveAdmin.routes(self)
+
+  # Devise routes for regular users
   devise_for :users
-  # ActiveAdmin.routes(self)
 end
